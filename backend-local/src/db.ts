9d1e86f37b91358d64634f302f0215f3a00c9a4d
root <root@localhost.localdomain>
2025-04-22 18:42:30 +0800

    修改了db.ts文件

--- backend-local/src/db.ts
+++ backend-local/src/db.ts
@@ -1,5 +1,13 @@
+
+import { fileURLToPath } from 'url';
+import path from 'path';
 import Database from 'better-sqlite3';
 
-const db = new Database('assets.db');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);     // src 目录
 
+const db = new Database(path.join(__dirname, '../data/assets.db'));
 export default db;
+
+
+
